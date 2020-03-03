5fceddfedd58e78e3c698bde15a08eb6c9d5ad4d
Alexander Makarov <sam@rmcreative.ru>
2020-03-04 01:51:19 +0300

    Revert "fix: #13749 Yii opens db connection even when hits query cache"

--- framework/db/mysql/QueryBuilder.php
+++ framework/db/mysql/QueryBuilder.php
@@ -9,7 +9,6 @@ namespace yii\db\mysql;
 
 use yii\base\InvalidArgumentException;
 use yii\base\NotSupportedException;
-use yii\caching\CacheInterface;
 use yii\db\Exception;
 use yii\db\Expression;
 use yii\db\Query;
@@ -391,22 +390,7 @@ class QueryBuilder extends \yii\db\QueryBuilder
      */
     private function supportsFractionalSeconds()
     {
-        // use cache to prevent open mysql connection
-        // https://github.com/yiisoft/yii2/issues/13749#issuecomment-481657224
-        $key = [__METHOD__, $this->db->dsn];
-        $cache = null;
-        $schemaCache = ( \Yii::$app && is_string($this->db->schemaCache) ) ? \Yii::$app->get($this->db->schemaCache, false) : $this->db->schemaCache;
-        if ( $this->db->enableSchemaCache && $schemaCache instanceof CacheInterface ) {
-            $cache = $schemaCache;
-        }
-        $version = $cache ? $cache->get($key) : null;
-        if( !$version ) {
-            $version = $this->db->getSlavePdo()->getAttribute(\PDO::ATTR_SERVER_VERSION);
-            if( $cache ) {
-                $cache->set($key, $version, $this->db->schemaCacheDuration);
-            }
-        }
-        
+        $version = $this->db->getSlavePdo()->getAttribute(\PDO::ATTR_SERVER_VERSION);
         return version_compare($version, '5.6.4', '>=');
     }
 
